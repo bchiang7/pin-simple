ea1af47a3e553e0e155f42d026e4a2ae5fbe82cf
Brittany Chiang <brittany.chiang@gmail.com>
2019-02-15 00:04:55 -0500

    Use express static

--- server/index.js
+++ server/index.js
@@ -26,8 +26,11 @@ const request = require('request');
 
 const app = express();
 
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, '../client/dist')));
+
 app
-  .use(serveStatic(path.join(__dirname, 'client/dist')))
+  .use(express.static(`../${__dirname}/client/dist`))
   .use(cors())
   .use(cookieParser());
 
